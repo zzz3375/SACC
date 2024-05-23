38883ed79cdfe9e227d46e15bcded27a2b7045b0
zzz3375 <13694863375@163.com>
2024-05-23 16:10:17 +0800

    1

--- crack_analysis.py
+++ crack_analysis.py
@@ -6,28 +6,31 @@ import torch
 import cv2
 import numpy as np
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 
-data_path = Path("./concrete_crack");data_path.mkdir(parents=1,exist_ok=1)
-out_path = Path("./crack_mask");out_path.mkdir(parents=1,exist_ok=1)
+data_path = Path("./data/concrete_crack");data_path.mkdir(parents=1,exist_ok=1)
+out_path = Path("./out/crack_mask");out_path.mkdir(parents=1,exist_ok=1)
 sam_checkpoint = Path("./sam_vit_h_4b8939.pth")
 
+def edge_supression(mask, e=8):
+    h,w=mask.shape
+    out = np.zeros_like(mask)
+    out[e:h-e,e:w-e] = mask[e:h-e,e:w-e]
+    return out
+
+
 if __name__=="__main__":
-    torch.cuda.empty_cache()  
     
     model_type = "vit_h"
     device = "cuda"
     sam = sam_model_registry[model_type](checkpoint=str(sam_checkpoint))
     sam.to(device=device)
-
     mask_generator = SamAutomaticMaskGenerator(sam)
     def area_criteria(mask):
         return mask["area"]>50_000
-
-    torch.cuda.empty_cache() 
-    # os.mkdir("../crack_mask")
     for im_name in os.listdir(data_path):
-        mask_generator = SamAutomaticMaskGenerator(sam)
+        
         img = cv2.imread(str(data_path/im_name))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         masks = mask_generator.generate(img)
@@ -38,7 +41,10 @@ if __name__=="__main__":
         for mask in filtered_masks:
             crack = np.bitwise_or( crack, mask["segmentation"]) 
         crack = np.bitwise_not(crack).astype(int)*255
+        crack = edge_supression(crack)
         cv2.imwrite(str(out_path/im_name), crack)   
-        del mask_generator
-        torch.cuda.empty_cache()     
+        # del mask_generator
+        # del masks
+        # torch.cuda.empty_cache()
+    
     
