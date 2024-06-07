4dfa29376fd77b6e7fd318ecf8546485a657850f
zzz3375 <13694863375@163.com>
2024-06-07 09:51:17 +0800

    1

--- yolo_suggestion.py
+++ yolo_suggestion.py
@@ -8,7 +8,7 @@ model = YOLO(r"C:\Users\13694\yolov8-adaptated\runs\segment\train-achieved\weigh
 source = r"data\concrete_crack\0.jpg"
 results = model(source)
 mask_raw = results[0].masks[0].cpu().data.numpy().transpose(1,2,0).astype(int)*255 
-cv2.imwrite("result.jpg",mask_raw)
+cv2.imwrite(r"tmp\yolo_raw_result.jpg",mask_raw)
 
 cmd = f"yolo segment predict model={model} source={source}"
 #%%
