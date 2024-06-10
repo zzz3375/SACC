4809064e4b6bed387f3dfb63d7d56d0c9a31db33
zzz3375 <13694863375@163.com>
2024-06-10 17:56:35 +0800

    1

--- complementary.py
+++ complementary.py
@@ -5,7 +5,8 @@ from segment_anything import sam_model_registry, SamAutomaticMaskGenerator
 import cv2
 import numpy as np
 from pathlib import Path
-# import matplotlib.pyplot as plt
+from PIL import Image
+import matplotlib.pyplot as plt
 
 #%%
 def edge_supression(mask):
@@ -15,33 +16,33 @@ def edge_supression(mask):
     out[dh:h-dh,dw:w-dw] = mask[dh:h-dh,dw:w-dw]
     return out
 
-def sam_seg_crack_area(img):
+def sam_seg_crack_comp(source):
     model_type = "vit_h"
     device = "cuda"
     
+    # img = cv2.imread(source)
+    img = np.asarray(Image.open(source))
     img_area = np.prod(img.shape[:2])
-    sam = sam_model_registry[model_type](checkpoint=str(sam_checkpoint))
+    sam = sam_model_registry[model_type](checkpoint="sam_vit_h_4b8939.pth")
     sam.to(device=device)
-    mask_generator = SamAutomaticMaskGenerator(sam)
+    mask_generator = SamAutomaticMaskGenerator(sam,min_mask_region_area=img_area*0.15)
     masks = mask_generator.generate(img)
     
-    filtered_masks = filter(lambda  mask: (mask["area"]/ img_area) > 0.15, masks)
+    filtered_masks = filter(lambda  mask:  (mask["area"]/ img_area) < 0.95, masks)
     
     crack = np.ones_like(masks[0]["segmentation"], dtype=bool)
-    for mask in filtered_masks:
+    for i, mask in enumerate(filtered_masks):
         crack[mask["segmentation"]]=False
-    crack = edge_supression(crack)
-    
-    return crack.astype(int)*255
+        cv2.imwrite(f"tmp\mask_{i}.jpg",mask["segmentation"].astype(int)*255)
 
+    crack = edge_supression(crack).astype(int)*255
+    cv2.imwrite(r"tmp\sam_comp_result.jpg",crack)
+    
+    return crack
+#%%
 if __name__=="__main__":
-    data_path = Path("./data/concrete_crack");data_path.mkdir(parents=1,exist_ok=1)
-    out_path = Path("./out/crack_mask");out_path.mkdir(parents=1,exist_ok=1)
-    sam_checkpoint = Path("./sam_vit_h_4b8939.pth")    
-    for im_name in os.listdir(data_path):
-        
-        img = cv2.imread(str(data_path/im_name))
-        crack = sam_quantify_crack(img)
-        cv2.imwrite(str(out_path/im_name), crack)   
+    source = r"data\crack_dataset_cleaned\混凝土桥梁裂缝optic_disc_seg\JPEGImages\H0016.jpg"
+    crack = sam_seg_crack_comp(source)
     
     
+# %%
