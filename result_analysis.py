cf42b0a5f97399fb10d3f760aecaa6df2318a944
zzz3375 <13694863375@163.com>
2024-06-24 23:26:37 +0800

    1

--- result_analysis.py
+++ result_analysis.py
@@ -1,23 +1,36 @@
 # %%
 import matplotlib.pyplot as plt 
+import matplotlib as mpl
+mpl.use('webAgg')
 import numpy as np
 # %%
 wid = np.load(r"tmp\width.npy")
 recall = np.load(r"tmp\rr.npy")
 recall_yolo = np.load(r"tmp\rr_yolo.npy")
-wid[wid>1000] = wid[wid>1000]/wid.max()*100
+wid[wid>30] = wid[wid>30]/wid.max()*10
 # %%
-plt.clf()
-plt.scatter(wid,recall)
-plt.scatter(wid,recall_yolo, marker="v")
-plt.show()
+# plt.clf()
+# plt.scatter(wid,recall)
+# plt.scatter(wid,recall_yolo, marker="v")
+# plt.show()
 # %%
 precision = np.load(r"tmp\pp.npy")
 precision_yolo = np.load(r"tmp\pp_yolo.npy")
 
+# # %%
+# plt.clf()
+# plt.scatter(wid,precision)
+# plt.scatter(wid,precision_yolo, marker="v")
+# plt.show()
 # %%
 plt.clf()
-plt.scatter(wid,precision)
-plt.scatter(wid,precision_yolo, marker="v")
+plt.stem(wid,recall - recall_yolo)
+print(np.sum((recall-recall_yolo)>0)/len(wid))
+print(recall.mean(),recall_yolo.mean())
+print(recall[wid > 10].mean(),recall_yolo[wid > 10].mean())
+print(precision.mean(),precision_yolo.mean())
+print(precision[wid > 10].mean(),precision_yolo[wid > 10].mean())
+plt.xlabel("crack width (pixel)")
+plt.title("recall_proposed - recall_yolo")
+plt.legend()
 plt.show()
-# %%
