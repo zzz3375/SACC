b1ee2ffa2ee89cc4fd318bd702500ed68f36d64f
zzz3375 <13694863375@163.com>
2024-07-03 17:41:48 +0800

    1

--- result_analysis.py
+++ result_analysis.py
@@ -5,34 +5,37 @@ import matplotlib.pyplot as plt
 import numpy as np
 from pathlib import Path
 # %%
-tmpdir = Path("tmp-se-best")
+tmpdir = Path(r"archived_result\tmp-yolo-best")
 wid = np.load(tmpdir/"width.npy")
 recall = np.load(tmpdir/"rr.npy")
 recall_yolo = np.load(tmpdir/"rr_yolo.npy")
 wid[wid>30] = wid[wid>30]/wid.max()*10
-# %%
-# plt.clf()
-# plt.scatter(wid,recall)
-# plt.scatter(wid,recall_yolo, marker="v")
-# plt.show()
-# %%
-precision = np.load(r"tmp\pp.npy")
-precision_yolo = np.load(r"tmp\pp_yolo.npy")
-
-# # %%
-# plt.clf()
-# plt.scatter(wid,precision)
-# plt.scatter(wid,precision_yolo, marker="v")
-# plt.show()
-# %%
-plt.clf()
-plt.stem(wid,recall - recall_yolo)
+precision = np.load(tmpdir/"pp.npy")
+precision_yolo = np.load(tmpdir/"pp_yolo.npy")
 print(np.sum((recall-recall_yolo)>0)/len(wid))
 print(np.sum((precision-precision_yolo)>0)/len(wid))
 print(recall.mean(),recall_yolo.mean())
 print(recall[wid > 10].mean(),recall_yolo[wid > 10].mean())
 print(precision.mean(),precision_yolo.mean())
 print(precision[wid > 10].mean(),precision_yolo[wid > 10].mean())
-plt.xlabel("crack width (pixel)")
-plt.title("recall_proposed - recall_yolo")
-plt.show()
+#%%
+def show_recall_improvements():
+    plt.clf()
+    plt.stem(wid,recall - recall_yolo)
+
+    plt.xlabel("Crack Width in Pixel")
+    # plt.title("recall_proposed - recall_yolo")
+    plt.ylabel("Improvements on Recall Rate")
+    # plt.show()
+
+    width = np.load("training_width.npy")
+    xmin= np.min(width)
+    xmax = np.max(width)
+    y = -0.3
+    color = "black"
+    plt.plot([xmin, xmax], [y, y], color)
+    plt.plot([xmax,xmax], [-0.25,-0.35], color)
+    plt.plot([xmin, xmin], [-0.25, -0.35], color)
+    plt.show()
+
+# show_recall_improvements()
